ef6786913891cc880d98ba89cf51d38630837f1b
kolosokame02-ops <kolosokame02@gmail.com>
2025-10-31 14:00:46 +1000

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-// Sets current year in footer
-document.addEventListener('DOMContentLoaded', function(){
-  var yearSpan = document.getElementById('year');
-  if (yearSpan) yearSpan.textContent = new Date().getFullYear();
-});
