a590de27ef0b3e09be735d8bac9fa5308447ddb4
kolosokame02-ops <kolosokame02@gmail.com>
2025-10-31 13:15:33 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// Sets current year in footer
+document.addEventListener('DOMContentLoaded', function(){
+  var yearSpan = document.getElementById('year');
+  if (yearSpan) yearSpan.textContent = new Date().getFullYear();
+});
